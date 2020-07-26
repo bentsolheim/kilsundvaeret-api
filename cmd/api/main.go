ab88eed0c742a019294bbb9ffd0aaeb00818e27d
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-07-26 17:39:35 +0200

    Reading data directly from datalogger as a temp measure before storing in database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/bentsolheim/go-app-utils/rest"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/api/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/api/v1/current-temp", func(c *gin.Context) {
+		resp, err := http.Get("http://datalogger.kilsundvaeret.no/api/v1/logger/bua/readings")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, rest.WrapResponse(nil, err))
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, rest.WrapResponse(nil, err))
+			return
+		}
+		c.String(http.StatusOK, string(body))
 	})
 	r.Run(":9010")
 }
