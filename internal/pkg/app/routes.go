236707161c4ccd76b93a73311384c4d340264f26
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-07-31 14:08:30 +0200

    Exposing debug

--- internal/pkg/app/routes.go
+++ internal/pkg/app/routes.go
@@ -27,6 +27,21 @@ func CreateGinEngine(config AppConfig) *gin.Engine {
 			}
 			c.String(http.StatusOK, string(body))
 		})
+		v1.GET("/current-debug", func(c *gin.Context) {
+
+			resp, err := http.Get(fmt.Sprintf("%s/api/v1/logger/bua/debug", config.DataLoggerUrl))
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, rest.WrapResponse(nil, err))
+				return
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, rest.WrapResponse(nil, err))
+				return
+			}
+			c.String(http.StatusOK, string(body))
+		})
 	}
 
 	return r
