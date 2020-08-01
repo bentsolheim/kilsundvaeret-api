90203e918e7e7ef61a60ded8ad98e3d3d0920e60
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-08-01 11:20:41 +0200

    Added error counter

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/client_model v0.2.0
 	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- internal/pkg/app/routes.go
+++ internal/pkg/app/routes.go
@@ -6,7 +6,11 @@ import (
 	"github.com/Depado/ginprom"
 	"github.com/bentsolheim/go-app-utils/rest"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/log"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -46,10 +50,14 @@ func CreateGinEngine(config AppConfig) *gin.Engine {
 	p.AddCustomGauge("logger_battery_level", "The analog reading of the battery level converted to a percentage", []string{"loggerId"})
 	p.AddCustomGauge("logger_signal_strength", "The signal strength of the GSM connection on the last upload", []string{"loggerId"})
 	p.AddCustomGauge("logger_time_spent_millis", "The number of milli seconds spent on the last iteration", []string{"loggerId"})
+	errorCounter := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "logger_processing_errors", Help: "A counter of all the times sending data has failed",
+	}, []string{"loggerId"})
+	prometheus.MustRegister(errorCounter)
 	go func() {
 		for {
-			if updateMetrics(config, p) {
-				return
+			if err := updateMetrics(config, p, errorCounter); err != nil {
+				log.Debug(err)
 			}
 			time.Sleep(60 * time.Second)
 		}
@@ -96,15 +104,16 @@ func CreateGinEngine(config AppConfig) *gin.Engine {
 	return r
 }
 
-func updateMetrics(config AppConfig, p *ginprom.Prometheus) bool {
-	resp, err := http.Get(fmt.Sprintf("%s/api/v1/logger/bua/debug", config.DataLoggerUrl))
+func updateMetrics(config AppConfig, p *ginprom.Prometheus, errorCounter *prometheus.CounterVec) error {
+	debugUrl := fmt.Sprintf("%s/api/v1/logger/bua/debug", config.DataLoggerUrl)
+	resp, err := http.Get(debugUrl)
 	if err != nil {
-		return true
+		return errors.Wrap(err, fmt.Sprintf("failed loading debug data from [%s]", debugUrl))
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return true
+		return errors.Wrap(err, "error reading debug data body")
 	}
 	response := DebugResponse{}
 	_ = json.Unmarshal(body, &response)
@@ -115,5 +124,16 @@ func updateMetrics(config AppConfig, p *ginprom.Prometheus) bool {
 	signalStrength, _ := strconv.ParseFloat(response.Items.SignalStrength, 64)
 	_ = p.SetGaugeValue("logger_signal_strength", []string{"bua"}, signalStrength)
 	_ = p.SetGaugeValue("logger_time_spent_millis", []string{"bua"}, float64(response.Items.TimeSpent))
-	return false
+
+	errorCounterBua := errorCounter.WithLabelValues("bua")
+	value := readCounter(errorCounterBua)
+	toAdd := float64(response.Items.Errors) - value
+	errorCounterBua.Add(toAdd)
+	return nil
+}
+
+func readCounter(m prometheus.Counter) float64 {
+	pb := &dto.Metric{}
+	_ = m.Write(pb)
+	return pb.GetCounter().GetValue()
 }
